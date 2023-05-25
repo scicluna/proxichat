361bc84037ecb23202ec0984b8888b357c848530
scicluna <88948700+scicluna@users.noreply.github.com>
2023-05-24 20:08:18 -0400

    geolocation range util function

--- utils/getAcceptableRanges.ts
+++ utils/getAcceptableRanges.ts
@@ -1,6 +1,19 @@
 
 
 export default function getAcceptableRanges(range: number, lat: number, lon: number) {
+    const R = 3958.8; // Earth's radius in miles
 
-    return { minLatitude: 5, maxLatitude: 10, minLongitude: 1, maxLongitude: 10 }
+    // Change in coordinates
+    let deltaLat = range / R;
+    let deltaLon = range / (R * Math.cos(Math.PI * lat / 180));
+
+    // Minimum and maximum latitudes for bounding box
+    let minLat = lat - deltaLat;
+    let maxLat = lat + deltaLat;
+
+    // Minimum and maximum longitudes for bounding box
+    let minLon = lon - deltaLon;
+    let maxLon = lon + deltaLon;
+
+    return { minLatitude: minLat, maxLatitude: maxLat, minLongitude: minLon, maxLongitude: maxLon }
 }
